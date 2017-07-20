4ed4273c496812b06c122171f71c041d2b76bb51
Ecalle <alan.ecalle@laposte.net>
2017-07-20 09:21:18 +0200

    Mise en place du formulaire d'observation

--- src/AppBundle/Controller/observationController.php
+++ src/AppBundle/Controller/observationController.php
@@ -6,15 +6,34 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Form\FormBuilder;
 
 class observationController extends Controller
 {
     /**
-     * @Route("/", name="core_home")
+     * @Route("/observation", name="app_observation")
      * @Method("GET")
      */
-    public function indexAction()
+    public function formAction()
     {
-        return $this->render('AppBundle:Default:index.html.twig');
+		$observation = new Observation();
+		$formBuilder = $this->get('form.factory')->create(CommandeType::class, $observation);
+
+		if ($request->isMethod('POST')) {
+			$formBuilder->handleRequest($request);
+
+			if ($formBuilder->isValid()) {
+				$em = $this->getDoctrine()->getManager();
+
+				$em->persist($commande);
+				$em->flush();     
+			}
+
+			return $this->redirectToRoute('');
+		}
+
+		return $this->render('AppBundle:Observation:observation.html.twig', array(
+		'form' => $formBuilder->createView()
+		));
     }
 }
