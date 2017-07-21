e507bd7a4739b8c46258329f4bf369df96273be1
Ecalle <alan.ecalle@laposte.net>
2017-07-21 08:24:00 +0200

    Mise en place des champs: comment, birdname, date

--- src/AppBundle/Form/ObservationType.php
+++ src/AppBundle/Form/ObservationType.php
@@ -3,6 +3,8 @@ namespace App\Bundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,7 +13,17 @@ class ObservationType extends AbstractType
   public function buildForm(FormBuilderInterface $builder, array $options)
   {
     $builder
-      ->add('name', TextType::class);
+      ->add('comment', TextareaType::class, array(
+            'label' => 'COMMENTAIRE (Facultatif)'
+      ))
+      ->add('date', DateType::class, array(
+            'label' => 'DATE*',
+            'html5' => false,
+            'widget' => 'single_text',
+        ))
+      ->add('birdName', TextType::class, array(
+                'label' => 'NOM DE L\'OISEAU'
+      ));
   }
 
   public function configureOptions(OptionsResolver $resolver)
