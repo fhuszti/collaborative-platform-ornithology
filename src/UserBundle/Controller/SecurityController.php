a3124a8f1b830e2c05fc1e204fd9507431b1f113
fhuszti <f.huszti@gmail.com>
2017-07-30 22:03:15 +0200

    Mise en place du layout principal et du fichier de contenu

--- src/UserBundle/Controller/SecurityController.php
+++ src/UserBundle/Controller/SecurityController.php
@@ -2,34 +2,18 @@
 
 namespace UserBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends Controller
 {
     /**
-     * @Route("/connexion", name="core_login")
+     * @Route("/connexion", name="core_home")
      * @Method("GET")
-     * @Security("has_role('IS_AUTHENTICATED_REMEMBERED')")
      */
-    public function loginAction(Request $request)
+    public function loginAction()
     {
-        // Si le visiteur est déjà identifié, on le redirige vers l'accueil
-	    if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-	      	return $this->redirectToRoute('core_home');
-	    }
-
-	    // Le service authentication_utils permet de récupérer le nom d'utilisateur
-	    // et l'erreur dans le cas où le formulaire a déjà été soumis mais était invalide
-	    // (mauvais mot de passe par exemple)
-	    $authenticationUtils = $this->get('security.authentication_utils');
-
-	    return $this->render('security/login.html.twig', array(
-	      	'last_username' => $authenticationUtils->getLastUsername(),
-	      	'error'         => $authenticationUtils->getLastAuthenticationError(),
-	    ));
+        return $this->render('core/index.html.twig');
     }
 }
