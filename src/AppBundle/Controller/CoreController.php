52328f58389d26ca0d46dfb7be6968ed7018e880
Fady Michel <fadymichel.r@gmail.com>
2017-08-03 14:35:22 +0200

    ajout de la route

--- src/AppBundle/Controller/CoreController.php
+++ src/AppBundle/Controller/CoreController.php
@@ -16,4 +16,25 @@ class CoreController extends Controller
     {
         return $this->render('core/index.html.twig');
     }
+
+    /**
+     * @Route("/find", name="find")
+     */
+    public function findAction(Request $request)
+	{
+	    $defaultData = array('message' => 'find here');
+	    $form = $this->createFormBuilder($defaultData)
+	        ->add('name', TextType::class)
+	        ->add('find', SubmitType::class)
+	        ->getForm();
+
+	    $form->handleRequest($request);
+
+	    if ($form->isSubmitted() && $form->isValid()) {
+	        // data is an array with "name", "email", and "message" keys
+	        $data = $form->getData();
+	    }
+
+	    return $this->render('core/find.html.twig', array('form' => $form->createView()));
+	}
 }
