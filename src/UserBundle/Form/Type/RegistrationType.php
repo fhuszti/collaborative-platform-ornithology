343dc193568a9bd26a21d9d0e207b2238cd541b6
fhuszti <f.huszti@gmail.com>
2017-08-07 16:30:47 +0200

    Correction de bugs sur l'entité User et le formulaire d'inscription

--- src/UserBundle/Form/Type/RegistrationType.php
+++ src/UserBundle/Form/Type/RegistrationType.php
@@ -7,7 +7,6 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
