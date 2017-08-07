97341c30960742a8253ea7664d764e4a8b6da02d
Ecalle <alan.ecalle@laposte.net>
2017-08-07 16:29:29 +0200

    Modification requête Ajax

--- src/AppBundle/Controller/ObservationController.php
+++ src/AppBundle/Controller/ObservationController.php
@@ -12,6 +12,13 @@ use AppBundle\Service\FileUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Serializer; 
+use Symfony\Component\Serializer\Encoder\XmlEncoder; 
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
 
 class ObservationController extends Controller
 {
@@ -23,29 +30,39 @@ class ObservationController extends Controller
     {
 		$observation = new Observation();
 		$formBuilder = $this->get('form.factory')->create(ObservationType::class, $observation);
+if ($request->isXmlHttpRequest()) {
 
 		if ($request->isMethod('POST')) {
 			$formBuilder->handleRequest($request);
+			// $data = $formBuilder->getData();
+			// $encoders = array(new XmlEncoder(), new JsonEncoder()); 
+			// $normalizers = array(new ObjectNormalizer()); 
+			// $serializer = new Serializer($normalizers, $encoders);
+			// $jsonContent = $serializer->serialize($data, 'json');
+			 
+			// return  new JsonResponse($jsonContent);
 
 			if ($formBuilder->isValid()) {
-				$observation->getImage()->upload();      
+				// $observation->getImage()->upload();      
 				$observation->setSeen(0);
 				$em = $this->getDoctrine()->getManager();
-
 				$em->persist($observation);
 				$em->flush();
 
-				$dir = '%kernel.project_dir%/web/uploads/images/';
-				$image = $observation->getImage()->getImage();
-				\Tinify\setKey("YN-tD6vaVHxYTx8XcfBLKFrlzXwwxgLi");
-				$source = \Tinify\fromFile($dir.$image);
-				$source->toFile($dir."/optimized/".$image);
+				// $dir = '%kernel.project_dir%/web/uploads/images/';
+				// $image = $observation->getImage()->getImage();
+				// \Tinify\setKey("YN-tD6vaVHxYTx8XcfBLKFrlzXwwxgLi");
+				// $source = \Tinify\fromFile($dir.$image);
+				// $source->toFile($dir."/optimized/".$image);
 
-				unlink('%kernel.project_dir%/web/uploads/images/'.$image ) ;   
+				// unlink('%kernel.project_dir%/web/uploads/images/'.$image ) ; 
+				return new JsonResponse(array('status'=>'success'));
 			}
-
+}
 			// return $this->redirectToRoute('');
 		}
+
+	
 
 		return $this->render('AppBundle:Observation:observation.html.twig', array(
 		'form' => $formBuilder->createView()
