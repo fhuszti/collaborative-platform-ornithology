6c9b6620cf2b9dfcad49732ae3063c6356f68624
Fady Michel <fadymichel.r@gmail.com>
2017-08-29 14:44:06 +0200

    rien de modifié

--- src/AppBundle/Controller/CoreController.php
+++ src/AppBundle/Controller/CoreController.php
@@ -5,6 +5,11 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\FormBuilder;
+use AppBundle\Entity\Contact;
+use AppBundle\Form\ContactType;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class CoreController extends Controller
 {
